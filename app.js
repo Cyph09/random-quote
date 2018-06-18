8fdadcb7e0bc8061b523dae0252eb5d629b1f3fe
Swaleh Mhina <cyph09@gmail.com>
2018-06-18 18:30:12 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Get ui elements
+const quoteBox = document.querySelector('#quote-box');
+const quote    = document.querySelector('#text');
+const author   = document.querySelector('#author');
+const newQuote = document.querySelector('#new-quote');
+
+// Event listener for  new quote
+window.addEventListener('load', displayQuote);
+quoteBox.addEventListener('click',displayQuote);
+
+function displayQuote(){
+  // Get quote from api and display in ui
+  fetch('https://talaikis.com/api/quotes/random/')
+  .then((res)=>res.json())
+  .then((data)=>{
+    quote.textContent = data.quote;
+    author.className = 'blockquote-footer text-right';
+    author.textContent = data.author;
+  })
+  .catch((err)=>{
+    quote.className = 'alert alert-danger';
+    quote.textContent = 'Something went wrong...'
+    console.log(err);
+  });
+}
